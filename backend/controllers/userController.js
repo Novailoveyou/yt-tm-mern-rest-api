54d41c5ea187d897a4aba66a8beebb3aa97c50ec
novailoveyou <novailoveyoux@gmail.com>
2022-02-12 14:19:18 +0300

    generate jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,12 @@ const registerUser = asyncHandler(async (req, res) => {
   })
 
   if (user) {
-    res.status(201).json({ _id: user.id, name: user.name, email: user.email })
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user.id)
+    })
   } else {
     res.status(400)
     throw new Error('Invalid user data')
@@ -54,7 +59,8 @@ const loginUser = asyncHandler(async (req, res) => {
     res.json({
       _id: user.id,
       name: user.name,
-      email: user.email
+      email: user.email,
+      token: generateToken(user.id)
     })
   } else {
     res.status(400)
@@ -68,6 +74,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: 'User data display' })
 })
+
+// Generate JWT
+const generateToken = id => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '30d'
+  })
+}
 
 module.exports = {
   registerUser,
